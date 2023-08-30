b9d4baf2e3b653338c4d57a1ccffc1a29fa0ab73
Usama Zulfiqar <usama.sama@gmail.com>
2023-08-31 00:12:12 +0500

    accountinfo added

--- src/resolvers/Payout/productInfo.js
+++ src/resolvers/Payout/productInfo.js
@@ -1,8 +1,8 @@
 export default async function productInfo(parent, args, context, info) {
   const productid = parent.productId;
-  const { Catalog } = context.collections;
-  const productInfo = await Catalog.findOne({ "product._id": productid });
+  const { Products } = context.collections;
+  const productInfo = await Products.findOne({ "_id": productid });
 
-  // console.log("productInfo", productInfo);
+  console.log("productInfo", productInfo);
   return productInfo;
 }
--- src/resolvers/Payout/sellerInfo.js
+++ src/resolvers/Payout/sellerInfo.js
@@ -2,5 +2,11 @@ export default async function sellerInfo(parent, args, context, info) {
   const sellerIds = parent.sellerId;
   const { Accounts } = context.collections;
   const sellerinfos = await Accounts.findOne({ _id: sellerIds });
- return sellerinfos;
+  console.log("sellerinfos",sellerinfos);
+  let response={
+    phone:sellerinfos?.phoneNumber?sellerinfos?.phoneNumber:sellerinfos?.contactNumber?sellerinfos?.contactNumber:sellerinfos?.billing.phone,
+    name:sellerinfos?.name?sellerinfos?.name:sellerinfos?.profile?.name?sellerinfos?.profile?.name:sellerinfos?.storeName,
+    accountDetails:{bankName:sellerinfos?.bankDetail?.bankName,bankAccountNumber:sellerinfos?.bankDetail?.bankAccountNumber}
+  }
+  return response;
 }
