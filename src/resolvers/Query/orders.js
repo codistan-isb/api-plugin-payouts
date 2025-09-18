9a88e49f4162de427bd0c4d38296250b473a2c57
MRamzan316 <muhammad.ramzan@arclabsolutions.com>
2025-09-18 12:05:26 +0500

    Fixes:Add the isPaid logic when the PaymentMethod is JAZZCASH

--- src/resolvers/Query/orders.js
+++ src/resolvers/Query/orders.js
@@ -1,33 +1,60 @@
-import getPaginatedResponse from "@reactioncommerce/api-utils/graphql/getPaginatedResponse.js";
-import wasFieldRequested from "@reactioncommerce/api-utils/graphql/wasFieldRequested.js";
+// import getPaginatedResponse from "@reactioncommerce/api-utils/graphql/getPaginatedResponse.js";
+// import wasFieldRequested from "@reactioncommerce/api-utils/graphql/wasFieldRequested.js";
+// import { decodeShopOpaqueId } from "../../xforms/id.js";
+
+// /**
+//  * @name Query/orders
+//  * @method
+//  * @memberof Order/GraphQL
+//  * @summary Get an order by its reference ID
+//  * @param {Object} parentResult - unused
+//  * @param {ConnectionArgs} args - An object of all arguments that were sent by the client
+//  * @param {Object} args.filters - An Object of filters to apply
+//  * @param {String} args.shopIds - shop IDs to check for orders from
+//  * @param {Object} context - An object containing the per-request state
+//  * @param {Object} info Info about the GraphQL request
+//  * @returns {Promise<Object>|undefined} An Order object
+//  */
+// export default async function orders(parentResult, args, context, info) {
+//   const { filters, shopIds: opaqueShopIds, ...connectionArgs } = args;
+
+//   const shopIds = opaqueShopIds && opaqueShopIds.map(decodeShopOpaqueId);
+
+//   const query = await context.queries.orders(context, {
+//     filters,
+//     shopIds
+//   });
+
+//   return getPaginatedResponse(query, connectionArgs, {
+//     includeHasNextPage: wasFieldRequested("pageInfo.hasNextPage", info),
+//     includeHasPreviousPage: wasFieldRequested("pageInfo.hasPreviousPage", info),
+//     includeTotalCount: wasFieldRequested("totalCount", info)
+//   });
+// }
+
 import { decodeShopOpaqueId } from "../../xforms/id.js";
 
 /**
- * @name Query/orders
- * @method
- * @memberof Order/GraphQL
- * @summary Get an order by its reference ID
- * @param {Object} parentResult - unused
- * @param {ConnectionArgs} args - An object of all arguments that were sent by the client
- * @param {Object} args.filters - An Object of filters to apply
- * @param {String} args.shopIds - shop IDs to check for orders from
- * @param {Object} context - An object containing the per-request state
- * @param {Object} info Info about the GraphQL request
- * @returns {Promise<Object>|undefined} An Order object
+ * Returns { nodes, totalCount, pageInfo } using custom pagination.
  */
-export default async function orders(parentResult, args, context, info) {
-  const { filters, shopIds: opaqueShopIds, ...connectionArgs } = args;
+export default async function orders(parent, args, context) {
+  const { filters, shopIds: opaqueShopIds, first = 10, offset = 0 } = args;
 
-  const shopIds = opaqueShopIds && opaqueShopIds.map(decodeShopOpaqueId);
+  const shopIds = opaqueShopIds?.map?.(decodeShopOpaqueId);
 
-  const query = await context.queries.orders(context, {
+  const { nodes, totalCount } = await context.queries.orders(context, {
     filters,
-    shopIds
+    shopIds,
+    first,
+    offset,
   });
 
-  return getPaginatedResponse(query, connectionArgs, {
-    includeHasNextPage: wasFieldRequested("pageInfo.hasNextPage", info),
-    includeHasPreviousPage: wasFieldRequested("pageInfo.hasPreviousPage", info),
-    includeTotalCount: wasFieldRequested("totalCount", info)
-  });
+  const pageInfo = {
+    hasPreviousPage: offset > 0,
+    hasNextPage: offset + nodes.length < totalCount,
+    startOffset: offset,
+    endOffset: offset + nodes.length - 1,
+  };
+
+  return { nodes, totalCount, pageInfo };
 }
